c6edf3b50e3099b85f97ad57e9b9c0752278c59d
Adrien ROBION <arobion@e2r3p6.42.fr>
2019-05-03 12:08:22 +0200

    add move to rubik class

--- rubik_cubes.py
+++ rubik_cubes.py
@@ -12,21 +12,152 @@ class Rubik():
         ret += "    {}        {}  {}        {}\n   {}  {}                {}  {}\n    {}        {}  {}        {}\n".format(self.get_edge_by_position(1).final_position,self.get_edge_by_position(8).final_position, self.get_edge_by_position(5).final_position, self.get_edge_by_position(9).final_position, self.get_edge_by_position(4).final_position, self.get_edge_by_position(2).final_position, self.get_edge_by_position(12).final_position, self.get_edge_by_position(10).final_position, self.get_edge_by_position(3).final_position, self.get_edge_by_position(7).final_position, self.get_edge_by_position(6).final_position, self.get_edge_by_position(11).final_position)
         return ret
 
-    def get_corner_by_number(self, number):
-        for elem in self.corners:
-            if number == elem.final_position:
-                return elem
-
-    def get_edge_by_number(self, number):
-        for elem in self.edges:
-            if number == elem.final_position:
-                return elem
+#    def get_corner_by_number(self, number):
+#        for elem in self.corners:
+#            if number == elem.final_position:
+#                return elem
+#
+#    def get_edge_by_number(self, number):
+#        for elem in self.edges:
+#            if number == elem.final_position:
+#                return elem
 
     def get_corner_by_position(self, position):
         return self.corners[position]
 
     def get_edge_by_position(self, position):
         return self.edges[position]
+
+    
+    def move(self, instruction):
+        #U
+        #corner: 1 2 3 4, edges: 1 2 3 4
+        if instruction == 1:
+            self.corners[1], self.corners[2], self.corners[3], self.corners[4] = self.corners[4], self.corners[1], self.corners[2], self.corners[3]
+            self.edges[1], self.edges[2], self.edges[3], self.edges[4] = self.edges[4], self.edges[1], self.edges[2], self.edges[3]
+        #R
+        #corner: 1 5 6 2, edges: 2 5 10 6
+        elif instruction == 2:
+            self.corners[2].orientation, self.corners[5].orientation = (self.corners[2].orientation + 1) % 3, (self.corners[5].orientation + 1) % 3
+            self.corners[1].orientation, self.corners[6].orientation = (self.corners[1].orientation - 1) % 3, (self.corners[6].orientation - 1) % 3
+            
+            self.corners[1], self.corners[5], self.corners[6], self.corners[2] = self.corners[2], self.corners[1], self.corners[5], self.corners[6]
+            self.edges[2], self.edges[5], self.edges[10], self.edges[6] = self.edges[6], self.edges[2], self.edges[5], self.edges[10]
+        #L
+        #corner: 4 3 7 8, edges: 4 7 12 8
+        elif instruction == 3:
+            self.corners[4].orientation, self.corners[7].orientation = (self.corners[4].orientation + 1) % 3, (self.corners[7].orientation + 1) % 3
+            self.corners[3].orientation, self.corners[8].orientation = (self.corners[3].orientation - 1) % 3, (self.corners[8].orientation - 1) % 3
+            
+            self.corners[4], self.corners[3], self.corners[7], self.corners[8] = self.corners[8], self.corners[4], self.corners[3], self.corners[7]
+            self.edges[4], self.edges[7], self.edges[12], self.edges[8] = self.edges[8], self.edges[4], self.edges[7], self.edges[12]
+        #D
+        #corner: 5 8 7 6, edges: 9 12 11 10
+        elif instruction == 4:
+            self.corners[5], self.corners[8], self.corners[7], self.corners[6] = self.corners[6], self.corners[5], self.corners[8], self.corners[7]
+            self.edges[9], self.edges[12], self.edges[11], self.edges[10] = self.edges[10], self.edges[9], self.edges[12], self.edges[11]
+        #F
+        #corner: 3 2 6 7, edges: 3 6 11 7
+        elif instruction == 5:
+            self.corners[3].orientation, self.corners[6].orientation = (self.corners[3].orientation + 1) % 3, (self.corners[6].orientation + 1) % 3
+            self.corners[2].orientation, self.corners[7].orientation = (self.corners[2].orientation - 1) % 3, (self.corners[7].orientation - 1) % 3
+            
+            self.edges[3].orientation, self.edges[11].orientation = (self.edges[3].orientation + 1) % 2, (self.edges[11].orientation + 1) %2
+            self.edges[6].orientation, self.edges[7].orientation = (self.edges[6].orientation + 1) % 2, (self.edges[7].orientation + 1) %2
+            
+            self.corners[3], self.corners[2], self.corners[6], self.corners[7] = self.corners[7], self.corners[3], self.corners[2], self.corners[6]
+            self.edges[3], self.edges[6], self.edges[11], self.edges[7] = self.edges[7], self.edges[3], self.edges[6], self.edges[11]
+        #B
+        #corner: 1 4 8 5, edges: 1 8 9 5
+        elif instruction == 6:
+            self.corners[1].orientation, self.corners[8].orientation = (self.corners[1].orientation + 1) % 3, (self.corners[8].orientation + 1) % 3
+            self.corners[4].orientation, self.corners[5].orientation = (self.corners[4].orientation - 1) % 3, (self.corners[5].orientation - 1) % 3
+            
+            self.edges[1].orientation, self.edges[9].orientation = (self.edges[1].orientation + 1) % 2, (self.edges[9].orientation + 1) %2
+            self.edges[5].orientation, self.edges[8].orientation = (self.edges[5].orientation + 1) % 2, (self.edges[8].orientation + 1) %2
+            
+            self.corners[1], self.corners[4], self.corners[8], self.corners[5] = self.corners[5], self.corners[1], self.corners[4], self.corners[8]
+            self.edges[1], self.edges[8], self.edges[9], self.edges[5] = self.edges[5], self.edges[1], self.edges[8], self.edges[9]
+        #U'
+        #corner: 4 3 2 1, edges: 4 3 2 1
+        elif instruction == 7:
+            self.corners[4], self.corners[3], self.corners[2], self.corners[1] = self.corners[1], self.corners[4], self.corners[3], self.corners[2]
+            self.edges[4], self.edges[3], self.edges[2], self.edges[1] = self.edges[1], self.edges[4], self.edges[3], self.edges[2]
+        #R'
+        #corner: 2 6 5 1, edges: 6 10 5 2
+        elif instruction == 8:
+            self.corners[2].orientation, self.corners[5].orientation = (self.corners[2].orientation + 1) % 3, (self.corners[5].orientation + 1) % 3
+            self.corners[6].orientation, self.corners[1].orientation = (self.corners[6].orientation - 1) % 3, (self.corners[1].orientation - 1) % 3
+            
+            self.corners[2], self.corners[6], self.corners[5], self.corners[1] = self.corners[1], self.corners[2], self.corners[6], self.corners[5]
+            self.edges[6], self.edges[10], self.edges[5], self.edges[2] = self.edges[2], self.edges[6], self.edges[10], self.edges[5]
+        #L'
+        #corner: 8 7 3 4, edges: 8 12 7 4
+        elif instruction == 9:
+            self.corners[7].orientation, self.corners[4].orientation = (self.corners[7].orientation + 1) % 3, (self.corners[4].orientation + 1) % 3
+            self.corners[8].orientation, self.corners[3].orientation = (self.corners[8].orientation - 1) % 3, (self.corners[3].orientation - 1) % 3
+            
+            self.corners[8], self.corners[7], self.corners[3], self.corners[4] = self.corners[4], self.corners[8], self.corners[7], self.corners[3]
+            self.edges[8], self.edges[12], self.edges[7], self.edges[4] = self.edges[4], self.edges[8], self.edges[12], self.edges[7]
+        #D'
+        #corner: 6 7 8 5, edges: 10 11 12 9
+        elif instruction == 10:
+            self.corners[6], self.corners[7], self.corners[8], self.corners[5] = self.corners[5], self.corners[6], self.corners[7], self.corners[8]
+            self.edges[10], self.edges[11], self.edges[12], self.edges[9] = self.edges[9], self.edges[10], self.edges[11], self.edges[12]
+        #F'
+        #corner:7 6 2 3, edges: 7 11 6 3
+        elif instruction == 11:
+            self.corners[6].orientation, self.corners[3].orientation = (self.corners[6].orientation + 1) % 3, (self.corners[3].orientation + 1) % 3
+            self.corners[7].orientation, self.corners[2].orientation = (self.corners[7].orientation - 1) % 3, (self.corners[2].orientation - 1) % 3
+            
+            self.edges[3].orientation, self.edges[11].orientation = (self.edges[3].orientation + 1) % 2, (self.edges[11].orientation + 1) %2
+            self.edges[6].orientation, self.edges[7].orientation = (self.edges[6].orientation + 1) % 2, (self.edges[7].orientation + 1) %2
+            
+            self.corners[7], self.corners[6], self.corners[2], self.corners[3] = self.corners[3], self.corners[7], self.corners[6], self.corners[2]
+            self.edges[7], self.edges[11], self.edges[6], self.edges[3] = self.edges[3], self.edges[7], self.edges[11], self.edges[6]
+        #B'
+        #corner: 5 8 4 1, edges: 5 9 8 1
+        elif instruction == 12:
+            self.corners[1].orientation, self.corners[8].orientation = (self.corners[1].orientation + 1) % 3, (self.corners[8].orientation + 1) % 3
+            self.corners[4].orientation, self.corners[5].orientation = (self.corners[4].orientation - 1) % 3, (self.corners[5].orientation - 1) % 3
+            
+            self.edges[1].orientation, self.edges[9].orientation = (self.edges[1].orientation + 1) % 2, (self.edges[9].orientation + 1) %2
+            self.edges[5].orientation, self.edges[8].orientation = (self.edges[5].orientation + 1) % 2, (self.edges[8].orientation + 1) %2
+            
+            self.corners[5], self.corners[8], self.corners[4], self.corners[1] = self.corners[1], self.corners[5], self.corners[8], self.corners[4]
+            self.edges[5], self.edges[9], self.edges[8], self.edges[1] = self.edges[1], self.edges[5], self.edges[9], self.edges[8]
+        #U2
+        #corner: 1-3 4-2, edges: 1-3 4-2
+        elif instruction == 13:
+            self.corners[1], self.corners[3], self.corners[4], self.corners[2] = self.corners[3], self.corners[1], self.corners[2], self.corners[4]
+            self.edges[1], self.edges[3], self.edges[4], self.edges[2] = self.edges[3], self.edges[1], self.edges[4], self.edges[2]
+        #R2
+        #corner: 1-6 2-5, edges: 2-10 5-6
+        elif instruction == 14:
+            self.corners[1], self.corners[6], self.corners[2], self.corners[5] = self.corners[6], self.corners[1], self.corners[5], self.corners[2]
+            self.edges[2], self.edges[10], self.edges[5], self.edges[6] = self.edges[10], self.edges[2], self.edges[6], self.edges[5]
+        #L2
+        #corner: 4-7 3-8, edges: 4-12 7-8
+        elif instruction == 15:
+            self.corners[4], self.corners[7], self.corners[3], self.corners[8] = self.corners[7], self.corners[4], self.corners[8], self.corners[3]
+            self.edges[4], self.edges[12], self.edges[7], self.edges[8] = self.edges[12], self.edges[4], self.edges[8], self.edges[7]
+        #D2
+        #corner: 5-7 6-8, edges: 9-11 10-12
+        elif instruction == 16:
+            self.corners[5], self.corners[7], self.corners[6], self.corners[8] = self.corners[7], self.corners[5], self.corners[8], self.corners[6]
+            self.edges[9], self.edges[11], self.edges[10], self.edges[12] = self.edges[11], self.edges[9], self.edges[12], self.edges[10]
+        #F2
+        #corner: 2-7 3-6, edges: 3-11 6-7
+        elif instruction == 17:
+            self.corners[2], self.corners[7], self.corners[3], self.corners[6] = self.corners[7], self.corners[2], self.corners[6], self.corners[3]
+            self.edges[3], self.edges[11], self.edges[6], self.edges[7] = self.edges[11], self.edges[3], self.edges[7], self.edges[6]
+        #B2
+        #corner: 1-8 4-5, edges: 1-9 5-8
+        elif instruction == 18:
+            self.corners[1], self.corners[8], self.corners[4], self.corners[5] = self.corners[8], self.corners[1], self.corners[5], self.corners[4]
+            self.edges[1], self.edges[9], self.edges[5], self.edges[8] = self.edges[9], self.edges[1], self.edges[8], self.edges[5]
+
+
 
 
 

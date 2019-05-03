31ca9d39b624adec447a42b9014254fe8e1bc715
Adrien ROBION <arobion@e2r3p6.42.fr>
2019-05-03 17:25:12 +0200

    Add argparsing

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import argparse
+
 from rubik_cubes import Rubik
 from rubik_moves import move, move_by_notation
 from rubik_state import rubik_state
 
 def scramble(string, rubik):
-    move_by_notation(rubik, string)
+    valid_moves = ["U", "U'", "U2", "R', "R'", "R2', "L", "L'", "L2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"]
+    moves = string.split(" ")
+    for elem in moves:
+        if elem not in valid_moves:
+            raise Exception("Error unvalid move, look at usage for more information")
+        move_by_notation(rubik, elem)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("scramble", help="Please enter a scramble composed of following moves :<U, U', U2, R, R', R2, L, L', L2, D, D', D2, F, F', F2, B, B', B2> ")
+    args = parser.parse_args()
     rubik = Rubik()
-    scramble("U", rubik)
+    try:
+        scramble(args.scramble, rubik)
+    except Exception as e:
+        print(e)
+        return
     print(rubik)
 
 if __name__ == "__main__":

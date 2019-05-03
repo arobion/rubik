ae8b23c4a550f4cbc48d7dabdddb15e79f129697
terngkub <terngnattapol@gmail.com>
2019-05-03 17:55:28 +0200

    edit main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from rubik_cubes import Rubik
 from rubik_moves import move, move_by_notation
 from rubik_state import rubik_state
+from ida import IDA
 
 def scramble(string, rubik):
     move_by_notation(rubik, string)
@@ -8,8 +9,18 @@ def scramble(string, rubik):
 
 def main():
     rubik = Rubik()
+    start = rubik_state(rubik, 0)
+
     scramble("U", rubik)
-    print(rubik)
+    scramble("R", rubik)
+    print(rubik.heuristic(rubik))
+
+    print(start)
+    print(rubik.heuristic(start))
+
+    ida = IDA(start, rubik.heuristic)
+    print(ida.bound)
+    print(ida.path)
 
 if __name__ == "__main__":
     main()

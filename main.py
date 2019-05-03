5dd5e0194b44ba3f7a3a9fd36e78894094b7a7d0
Adrien ROBION <arobion@e2r3p6.42.fr>
2019-05-03 16:58:52 +0200

    Add tuple of state

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from rubik_cubes import Rubik
 from rubik_moves import move
+from rubik_state import rubik_state
 
 def main():
     rubik = Rubik()
-    move(rubik, 1)
-    print(rubik)
+#    move(rubik, 1)
+#    print(rubik)
+    stat = rubik_state(rubik, 0)
+    print(stat)
+    stat2 = rubik_state(stat, 1)
+    print(stat2.state)
 
 if __name__ == "__main__":
     main()
--- rubik_state.py
+++ rubik_state.py
@@ -1,18 +1,27 @@
-from rubik_cubes import Rubik
+from rubik_moves import move
 
 class rubik_state():
-    def __init__(self, Rubik, parent, instruction):
-        self.edges = parent.edges.copy()
-        self.corners = parent.corners.copy()
-        self.instruction = instruction
-        Rubik.move(instruction, self)
+    def __init__(self, parent, instruction):
+        if instruction == 0:
+            self.g = 0
+            self.edges = parent.edges.copy()
+            self.corners = parent.corners.copy()
+        else:
+            self.edges = parent.edges.copy()
+            self.corners = parent.corners.copy()
+            self.instruction = instruction
+            move(self, instruction)
 
-        self.g = parent.g + 1
-#        self.h = Rubik.heuristic_manhattan(self)
-#        self.f = self.g + self.h
+            self.g = parent.g + 1
 
-        self.state = self.construct_state()
+        self.state = tuple([(sub.final_position, sub.orientation) for dic in [self.corners, self.edges] for sub in dic.values()])
 
-    def construct_state(self):
-        pass
+    def __str__(self):
+        ret = "g(x): {}\n".format(self.g)
+        ret += "coins:\n"
+        ret += "\t{}  {}\t{}  {}\n\t U\t D\n\t{}  {}\t{}  {}\n".format(self.corners[4].final_position, self.corners[1].final_position, self.corners[8].final_position, self.corners[5].final_position, self.corners[3].final_position, self.corners[2].final_position, self.corners[7].final_position, self.corners[6].final_position)
+        ret += "aretes:\n"
+        ret += "    {}        {}  {}        {}\n   {}  {}                {}  {}\n    {}        {}  {}        {}\n".format(self.edges[1].final_position,self.edges[8].final_position, self.edges[5].final_position, self.edges[9].final_position, self.edges[4].final_position, self.edges[2].final_position, self.edges[12].final_position, self.edges[10].final_position, self.edges[3].final_position, self.edges[7].final_position, self.edges[6].final_position, self.edges[11].final_position)
+        return ret
+
 

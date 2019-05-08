aeca489393436fd9577a93593adb3164e31228b4
Adrien ROBION <arobion@e2r6p19.42.fr>
2019-05-08 15:01:25 +0200

    fix output

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import time
 
 from rubik_cubes import Rubik
-from rubik_moves import move, move_by_notation
+from rubik_moves import move, move_by_notation, move_translator
 from rubik_state import rubik_state
 from rubik_next import get_nexts_1, get_nexts_2
 from rubik_ida import IDA
@@ -31,10 +31,13 @@ def main():
     ida1 = IDA(phase1, rubik.heuristic_h1, get_nexts_1, {}) # empty pruning table
     ret1 = ida1.run()
 
+    total_moves = ""
     print("***** Phase 1 *****\n")
     for state in ret1:
         print(state)
         print("_______________________________________")
+        if state.instruction != 0:
+            total_moves += move_translator[state.instruction] + " "
 
     phase2 = rubik_state(ret1[-1], 0, rubik.heuristic_h2)
     ida2 = IDA(phase2, rubik.heuristic_h2, get_nexts_2, rubik.pruning_phase2)
@@ -44,6 +47,12 @@ def main():
     for state in ret2:
         print(state)
         print("_______________________________________")
+        if state.instruction != 0:
+            total_moves += move_translator[state.instruction] + " "
+
+    print("scramble: {}".format(args.scramble))
+    print("solution: {}".format(total_moves[:-1]))
+    print(len(ret1) + len(ret2) - 2)
 
 
 if __name__ == "__main__":

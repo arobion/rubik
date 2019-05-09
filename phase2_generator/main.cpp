51dfe70c61de7ee7b46ad173d58b5654aa1ad9c0
terngkub <terngnattapol@gmail.com>
2019-05-09 17:47:52 +0200

    finish basic c++ bfs

--- phase2_generator/main.cpp
+++ phase2_generator/main.cpp
@@ -1,39 +1,40 @@
 #include "State.hpp"
 #include "move.hpp"
 #include <iostream>
+#include <bitset>
+#include <unordered_map>
+#include <list>
+
+typedef std::unordered_map<std::bitset<72>, char> map_t;
+
+map_t bfs(State & start)
+{
+    map_t map;
+	map[start.compressed] = start.g;
+
+	std::list<State> queue;
+	queue.push_back(start);
+
+	while (queue.size())
+	{
+		auto current = queue.front();
+		queue.pop_front();
+		if (current.g >= 7)
+			continue;
+		for (auto next : current.get_nexts())
+		{
+			if (map.find(next->compressed) != map.end())
+				continue;
+			map[next->compressed] = next->g;
+			queue.push_back(*next);
+		}
+	}
+	return map;
+}
 
 int main(void)
 {
 	State s1{};
-	std::cout << s1 << std::endl;
-
-	auto s2 = move(s1, U);
-	std::cout << s2 << std::endl;
-
-	auto s3 = move(s1, UR);
-	std::cout << s3 << std::endl;
-
-	s2 = move(s1, U2);
-	std::cout << s2 << std::endl;
-
-	s2 = move(s1, D);
-	std::cout << s2 << std::endl;
-
-	s2 = move(s1, DR);
-	std::cout << s2 << std::endl;
-
-	s2 = move(s1, D2);
-	std::cout << s2 << std::endl;
-
-	s2 = move(s1, R2);
-	std::cout << s2 << std::endl;
-
-	s2 = move(s1, L2);
-	std::cout << s2 << std::endl;
-
-	s2 = move(s1, F2);
-	std::cout << s2 << std::endl;
-
-	s2 = move(s1, B2);
-	std::cout << s2 << std::endl;
+	auto map = bfs(s1);
+	std::cout << map.size() << std::endl;
 }

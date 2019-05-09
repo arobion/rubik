bf4716c806a86b596ce354bd9f8e926b1598a1f5
Adrien ROBION <arobion@e2r6p2.42.fr>
2019-05-09 16:47:01 +0200

    Add compression function

--- phase2_generator/State.cpp
+++ phase2_generator/State.cpp
@@ -10,21 +10,37 @@ State::State() :
 	edges.reserve(12);
 	for (auto i = 0; i < 12; ++i)
 		edges.push_back(i);
+	this->compressed = this->compress();
 }
 
 State::State(State const & origin) :
 	corners(origin.corners),
 	edges(origin.edges),
 	g(origin.g),
-	instruction(origin.instruction)
+	instruction(origin.instruction),
+	compressed(this->compress())
 {}
 
 State::State(State const & origin, Instruction instruction) :
 	corners(origin.corners),
 	edges(origin.edges),
 	g(origin.g),
-	instruction(instruction)
+	instruction(instruction),
+	compressed(this->compress())
 {}
+
+State & State::operator=(State const & rhs)
+{
+	if (this != &rhs)
+	{
+		this->corners = rhs.corners;
+		this->edges = rhs.edges;
+		this->g = rhs.g;
+		this->instruction = rhs.instruction;
+		this->compressed = rhs.compress();
+	}
+	return *this;
+}
 
 std::ostream & operator<<(std::ostream & o, State const & rhs)
 {
@@ -38,5 +54,24 @@ std::ostream & operator<<(std::ostream & o, State const & rhs)
 	for (auto i = 0; i < 12; ++i)
 		o << int(rhs.edges[i]) << " ";
 	o << std::endl;
+	o << "compressed:" << std::endl;
+	o << rhs.compressed << std::endl;
+	o << std::endl;
 	return o;
+}
+
+std::bitset<72> State::compress() const
+{
+	std::bitset<72> ret;
+	for(char i = 0;i < 8; i++)
+	{
+		ret <<= 3;
+		ret |= this->corners[i];
+	}
+	for(char i = 0;i < 12; i++)
+	{
+		ret <<= 4;
+		ret |= this->edges[i];
+	}
+	return ret;
 }

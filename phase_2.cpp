00f374af170a1f79222717edad0f0cd86f18c707
Adrien ROBION <arobion@e2r6p2.42.fr>
2019-05-09 14:47:56 +0200

    add 10 G1 moves

--- phase_2.cpp
+++ phase_2.cpp
@@ -6,7 +6,7 @@
 /*   By: arobion <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 21:15:12 by arobion           #+#    #+#             */
-/*   Updated: 2019/05/09 14:12:14 by arobion          ###   ########.fr       */
+/*   Updated: 2019/05/09 14:44:29 by arobion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,7 @@ void mov(State & state, char instruction)
 State & u(State & origin)
 { 
 	State * state = new State(origin);
+	state->instruction = 1;
 
 	char tmp = state->corners[0];
 	state->corners[0] = state->corners[3];
@@ -97,6 +98,7 @@ State & u(State & origin)
 State & ur(State & origin)
 { 
 	State * state = new State(origin);
+	state->instruction = 7;
 
 	char tmp = state->corners[0];
 	state->corners[0] = state->corners[1];
@@ -115,6 +117,7 @@ State & ur(State & origin)
 State & u2(State & origin)
 { 
 	State * state = new State(origin);
+	state->instruction = 13;
 
 	char tmp = state->corners[0];
 	state->corners[0] = state->corners[2];
@@ -131,6 +134,165 @@ State & u2(State & origin)
 	tmp = state->edges[1];
 	state->edges[1] = state->edges[3];
 	state->edges[3] = tmp;
+
+	return *state;
+}
+
+State & d(State & origin)
+{ 
+	State * state = new State(origin);
+	state->instruction = 4;
+
+	char tmp = state->corners[4];
+	state->corners[4] = state->corners[5];
+	state->corners[5] = state->corners[6];
+	state->corners[6] = state->corners[7];
+	state->corners[7] = tmp;
+	tmp = state->edges[8];
+	state->edges[8] = state->edges[9];
+	state->edges[9] = state->edges[10];
+	state->edges[10] = state->edges[11];
+	state->edges[11] = tmp;
+
+	return *state;
+}
+
+State & dr(State & origin)
+{ 
+	State * state = new State(origin);
+	state->instruction = 10;
+
+	char tmp = state->corners[4];
+	state->corners[4] = state->corners[7];
+	state->corners[7] = state->corners[6];
+	state->corners[6] = state->corners[5];
+	state->corners[5] = tmp;
+	tmp = state->edges[8];
+	state->edges[8] = state->edges[11];
+	state->edges[11] = state->edges[10];
+	state->edges[10] = state->edges[9];
+	state->edges[9] = tmp;
+
+	return *state;
+}
+
+State & d2(State & origin)
+{ 
+	State * state = new State(origin);
+	state->instruction = 16;
+
+	char tmp = state->corners[4];
+	state->corners[4] = state->corners[6];
+	state->corners[6] = tmp;
+
+	tmp = state->corners[5];
+	state->corners[5] = state->corners[7];
+	state->corners[7] = tmp;
+
+	tmp = state->edges[8];
+	state->edges[8] = state->edges[10];
+	state->edges[10] = tmp;
+
+	tmp = state->edges[9];
+	state->edges[9] = state->edges[11];
+	state->edges[11] = tmp;
+
+	return *state;
+}
+
+State & r2(State & origin)
+{ 
+	State * state = new State(origin);
+	state->instruction = 14;
+
+	char tmp = state->corners[0];
+	state->corners[0] = state->corners[5];
+	state->corners[5] = tmp;
+
+	tmp = state->corners[1];
+	state->corners[1] = state->corners[4];
+	state->corners[4] = tmp;
+
+	tmp = state->edges[1];
+	state->edges[1] = state->edges[9];
+	state->edges[9] = tmp;
+
+	tmp = state->edges[5];
+	state->edges[5] = state->edges[4];
+	state->edges[4] = tmp;
+
+	return *state;
+}
+
+State & l2(State & origin)
+{ 
+	State * state = new State(origin);
+	state->instruction = 15;
+
+	char tmp = state->corners[3];
+	state->corners[3] = state->corners[6];
+	state->corners[6] = tmp;
+
+	tmp = state->corners[2];
+	state->corners[2] = state->corners[7];
+	state->corners[7] = tmp;
+
+	tmp = state->edges[3];
+	state->edges[3] = state->edges[11];
+	state->edges[11] = tmp;
+
+	tmp = state->edges[6];
+	state->edges[6] = state->edges[7];
+	state->edges[7] = tmp;
+
+	return *state;
+}
+
+State & f2(State & origin)
+{ 
+	State * state = new State(origin);
+	state->instruction = 17;
+
+	char tmp = state->corners[1];
+	state->corners[1] = state->corners[6];
+	state->corners[6] = tmp;
+
+	tmp = state->corners[2];
+	state->corners[2] = state->corners[5];
+	state->corners[5] = tmp;
+
+	tmp = state->edges[2];
+	state->edges[2] = state->edges[10];
+	state->edges[10] = tmp;
+
+	tmp = state->edges[6];
+	state->edges[6] = state->edges[5];
+	state->edges[5] = tmp;
+
+	return *state;
+}
+
+
+State & b2(State & origin)
+{ 
+	State * state = new State(origin);
+	state->instruction = 18;
+
+	char tmp = state->corners[0];
+	state->corners[0] = state->corners[7];
+	state->corners[7] = tmp;
+
+	tmp = state->corners[3];
+	state->corners[3] = state->corners[4];
+	state->corners[4] = tmp;
+
+	tmp = state->edges[0];
+	state->edges[0] = state->edges[8];
+	state->edges[8] = tmp;
+
+	tmp = state->edges[4];
+	state->edges[4] = state->edges[7];
+	state->edges[7] = tmp;
 
 	return *state;
 }
@@ -152,5 +314,19 @@ int main(void)
 	s2 = u2(s);
 	std::cout << s2<< std::endl;
 	s2 = ur(s);
+	std::cout << s2<< std::endl;
+	s2 = d(s);
+	std::cout << s2<< std::endl;
+	s2 = dr(s);
+	std::cout << s2<< std::endl;
+	s2 = d2(s);
+	std::cout << s2<< std::endl;
+	s2 = r2(s);
+	std::cout << s2<< std::endl;
+	s2 = l2(s);
+	std::cout << s2<< std::endl;
+	s2 = f2(s);
+	std::cout << s2<< std::endl;
+	s2 = b2(s);
 	std::cout << s2<< std::endl;
 }

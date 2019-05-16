ac7c1879cac9325501c2230ac36f79e5aa161207
Pierre Nardozi <pierrenardozi@Pierres-iMac.local>
2019-05-17 00:46:23 +0200

    finish visu

--- python/main.py
+++ python/main.py
@@ -79,68 +79,114 @@ def launch_cmd(cpp):
     print("Bye")
     cpp.kill()
 
+def convert_list2str(l):
+    res = ""
+    for s in l:
+        res += s
+        res += " "
+    res = res.strip()
+    return res
+
 def launch_visu(cpp):
     pg.init()
     size = width, height = 1200, 600
     window = pg.display.set_mode(size)
     backgound = pg.image.load("./resource/background.png").convert()
+    kociemba = pg.image.load("./resource/kociemba.jpg").convert()
     cube = RubikVisu(window)
-    input_box = InputBox(650, 240, 400, 32)
+    input_box = InputBox(650, 140, 400, 32)
     
     font_title = pg.font.Font("./resource/04B_30__.TTF", 50)
     font = pg.font.Font("./resource/04B_30__.TTF", 20)
+    font_result = pg.font.Font(None, 25)
     title = font_title.render("MEGA Rubik's solver 3000", True, (255, 0, 0))
     text_to_solve = font.render("Press S to solve", True, (0, 0, 0))
     text_to_reset = font.render("Press X to reset", True, (0, 0, 0))
     text_to_input = font.render("Enter number or scramble", True, (0, 0, 0))
+    text_to_solution_put = font.render("Currently:  (press key left or right)", True, (0, 0, 0))
     
     text = ""
     flag_scrambled = False
     solution = []
+    solution_put = []
+    melange = ""
     while 1:
         window.fill((30, 30, 30))
         window.blit(backgound, (0, 0))
+        window.blit(kociemba, (700, 240))
         window.blit(title, (100, 50))
-        window.blit(text_to_solve, (650, 160))
-        window.blit(text_to_reset, (650, 190))
-        window.blit(text_to_input, (650, 220))
+        window.blit(text_to_input, (650, 110))
+        window.blit(text_to_solve, (650, 180))
+        window.blit(text_to_reset, (650, 210))
         cube.put_cube2window(window)
+        if len(melange):
+            text_to_scramble = font.render("Scramble: (moves: " + str(len(melange.split())) + ")", True, (0, 0, 0))
+            window.blit(text_to_scramble, (400, 400))
+            text_scramble = font_result.render(melange, True, (0, 0, 0))
+            window.blit(text_scramble, (400, 430))
         if len(solution):
-            cube.make_move(solution.pop(0))
-            cube.put_cube2window(window)
-        else:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    cpp.kill()
-                    sys.exit()
-                else:
-                    if event.type == pg.KEYDOWN:
-                        if event.key == pg.K_s:
-                            print(flag_scrambled)
-                            if flag_scrambled == True:
-                                flag_scrambled = False
-                                solution = handle_cpp(cpp, rubik, cube.melange).split()
-                        if event.key == pg.K_x:
-                            cube.reset_visu()
+            text_to_solution = font.render("Solution: (moves: " + str(len(solution)) + ")", True, (0, 0, 0))
+            window.blit(text_to_solution, (400, 460))
+            text_solution = font_result.render(convert_list2str(solution), True, (0, 0, 0))
+            window.blit(text_solution, (400, 480))
+        if len(solution_put):
+            window.blit(text_to_solution_put, (400, 510))
+            text_solution_put = font_result.render(convert_list2str(solution_put), True, (0, 0, 0))
+            window.blit(text_solution_put, (400, 540))
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                cpp.kill()
+                sys.exit()
+            else:
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_s:
+                        if flag_scrambled == True:
+                            flag_scrambled = False
+                            solution = handle_cpp(cpp, rubik, melange).split()
+                            solution_put = solution
+                            cube.scramble(convert_list2str(solution))
                             cube.put_cube2window(window)
-                    text = input_box.handle_event(event)
-                    if text != "":
-                        rubik = Rubik()
+                    if event.key == pg.K_x:
+                        solution = []
+                        solution_put = []
+                        melange = ""
                         cube.reset_visu()
-                        try:
-                            number = int(text)
-                            text = random_scramble(number)
-                        except Exception as e:
-                            pass
-                        try:
-                            scramble(text, rubik)
-                            cube.scramble(text)
-                        except Exception as e:
-                            print("entrer un melange valide")
-                        cube.melange = text
-                        text = ""
                         cube.put_cube2window(window)
-                        flag_scrambled = True
+                    if event.key == pg.K_RIGHT and len(solution):
+                        if len(solution_put) != len(solution):
+                            solution_put = solution[:len(solution_put) + 1]
+                            cube.reset_visu()
+                            cube.scramble(melange)
+                            cube.scramble(convert_list2str(solution_put))
+                            cube.put_cube2window(window)
+                    if event.key == pg.K_LEFT and len(solution):
+                        if len(solution_put) != 0:
+                            solution_put = solution[:len(solution_put) - 1]
+                            cube.reset_visu()
+                            cube.scramble(melange)
+                            cube.scramble(convert_list2str(solution_put))
+                            cube.put_cube2window(window)
+                text = input_box.handle_event(event)
+                if text != "":
+                    rubik = Rubik()
+                    cube.reset_visu()
+                    try:
+                        number = int(text)
+                        text = random_scramble(number)
+                    except Exception as e:
+                        pass
+                    try:
+                        scramble(text, rubik)
+                        cube.scramble(text)
+                        solution = []
+                        solution_put = []
+                    except Exception as e:
+                        print("entrer un melange valide")
+                    melange = text
+                    text = ""
+                    cube.put_cube2window(window)
+                    flag_scrambled = True
         input_box.update()
         input_box.draw(window)
         pg.display.flip()

dffc7250982ebc544ecb0eb39823bf4c29abef29
Adrien ROBION <arobion@e2r2p4.42.fr>
2019-05-16 15:51:57 +0200

    Add auto scrambling option

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 import time
 import subprocess
 
@@ -8,6 +9,19 @@ from rubik_state import rubik_state
 from rubik_next import get_nexts_1, get_nexts_2
 from rubik_ida import IDA
 
+def random_scramble(number):
+    ret = ""
+    set_moves = ["U", "D", "R", "L", "F", "B", "U'", "D'", "R'", "L'", "F'", "B'", "U2", "D2", "R2", "L2", "F2", "B2"]
+    last_move = "E"
+    for i in range(number):
+        move = random.choice(set_moves)
+        while(move[0] == last_move[0]):
+            move = random.choice(set_moves)
+        ret += move + " "
+        last_move = move
+    return ret[:-1]
+
+
 def scramble(string, rubik):
     valid_moves = ["U", "U'", "U2", "R", "R'", "R2", "L", "L'", "L2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"]
     moves = string.split()
@@ -16,7 +30,7 @@ def scramble(string, rubik):
             raise Exception("Error unvalid move : {}, look at usage for more information".format(elem))
         move_by_notation(rubik, elem)
 
-def handle_cpp(cpp, rubik):
+def handle_cpp(cpp, rubik, instructions):
     corners = ""
     for i in range(1, 9):
         corners += str(rubik.corners[i].final_position - 1) + " "
@@ -31,7 +45,10 @@ def handle_cpp(cpp, rubik):
         eo += str(rubik.edges[i].orientation) + " "
 
     cpp.stdin.write("{}{}{}{}\n".format(corners, edges, co, eo))
+    start_time = time.time()
     solution = cpp.stdout.readline()
+    print("\nscramble: {}".format(instructions))
+    print("time resolution: {:.2f} s".format(time.time() - start_time))
     print("nb moves: {}".format(len(solution.split())))
     print("solution: {}".format(solution), end="")
 
@@ -50,17 +67,23 @@ def main():
         print("\t" + read, end="")
         read = cpp.stdout.readline()
     print("C++ program is Ready")
-    print("Please enter a scramble :")
+    print("\nPlease enter a specific scramble or a size for a random scramble:")
     instruction = input()
     while instruction != "exit":
+        rubik = Rubik()
         try:
-            rubik = Rubik()
+            number = int(instruction)
+            instruction = random_scramble(number)
+        except Exception as e:
+            pass
+        try:
             scramble(instruction, rubik)
-            handle_cpp(cpp, rubik)
+            handle_cpp(cpp, rubik, instruction)
         except Exception as e:
             print(e)
-        print("Please enter a scramble :")
+        print("\nPlease enter a specific scramble or a size for a random scramble:")
         instruction = input()
+    cpp.kill()
 
 if __name__ == "__main__":
     main()
